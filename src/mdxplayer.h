dde03aca277c1d9884e2e8a31f8f2c76ebd84d12
GOROman <yoshihito.kondoh@gmail.com>
2022-05-07 16:12:54 +0900

    ソースコードを分離・成形

--- src/mdxplayer.h
+++ src/mdxplayer.h
@@ -1,15 +1,6 @@
 #pragma once
-
-#include <math.h>
-#include <float.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <assert.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <libgen.h>		// for dirname()
-#include <unistd.h>
+#include <stdlib.h>
 
 #include <portaudio.h>
 
@@ -39,34 +30,6 @@ private:
     PaError _result;
 };
 
-
-
-// メモリ確保した領域にファイルを読み込む
-static void *mallocReadFile(
-	const char *fileName,
-	uint32_t *sizeRet
-){
-	FILE *fd = fopen(fileName, "rb");
-	if (fd == NULL) return NULL;
-	struct stat stbuf;
-	if (fstat(fileno(fd), &stbuf) == -1) {
-		fclose(fd);
-		return NULL;
-	}
-
-	assert(stbuf.st_size < 0x100000000LL);
-	uint32_t size = (uint32_t)stbuf.st_size;
-	void *buffer = malloc(size);
-	if (buffer == NULL) {
-		fclose(fd);
-		return NULL;
-	}
-	fread(buffer, 1, size, fd);
-	*sizeRet = size;
-	fclose(fd);
-	return buffer;
-}
-
 class MDXPlayer
 {
     MxdrvContext context;   // MXDRV Context
@@ -83,348 +46,22 @@ class MDXPlayer
 
 
 public:
-    MDXPlayer() : stream(0), mdxBuffer(NULL), pdxBuffer(NULL)
-    {
+    MDXPlayer();
+    virtual ~MDXPlayer();
 
-        // コンテキストの初期化
-        #define MDX_BUFFER_SIZE		1 * 1024 * 1024
-        #define PDX_BUFFER_SIZE		2 * 1024 * 1024
-        #define MEMORY_POOL_SIZE	8 * 1024 * 1024
-        if (MxdrvContext_Initialize(&context, MEMORY_POOL_SIZE) == false) {
-            printf("MxdrvContext_Initialize failed.\n");
-     //       return false;
-        }
+    bool open();
+    bool load(char* mdxFilePath);
+    bool close();
 
-    }
-    virtual ~MDXPlayer() {
-		MxdrvContext_Terminate(&context);
-
-    }
-
-    bool open(PaDeviceIndex index)
-    {
-        if( paInit.result() != paNoError ) {
-            printf("Error:paInit\n");
-		    return false;
-        }
-
-        PaStreamParameters outputParameters;
-
-        outputParameters.device = index;
-        if (outputParameters.device == paNoDevice) {
-            return false;
-        }
-
-        const PaDeviceInfo* pInfo = Pa_GetDeviceInfo(index);
-        if (pInfo != 0)
-        {
-//            printf("Output device name: %s\n", pInfo->name);
-        }
-
-        outputParameters.channelCount     = 2;       // stereo output 
-        outputParameters.sampleFormat     = paInt16;
-        outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-        outputParameters.hostApiSpecificStreamInfo = NULL;
-
-        PaError err = Pa_OpenStream(
-            &stream,
-            NULL, // no input 
-            &outputParameters,
-            SAMPLE_RATE,
-            paFramesPerBufferUnspecified,
-            paClipOff,      // we won't output out of range samples so don't bother clipping them 
-            &MDXPlayer::paCallback,
-            this            // Using 'this' for userData so we can cast to MDXPlayer* in paCallback method 
-            );
-
-        if (err != paNoError)
-        {
-            // Failed to open stream to device !!! 
-            return false;
-        }
-
-        err = Pa_SetStreamFinishedCallback( stream, &MDXPlayer::paStreamFinished );
-
-        if (err != paNoError)
-        {
-            Pa_CloseStream( stream );
-            stream = 0;
-
-            return false;
-        }
-
-        return true;
-    }
-
-    bool load( char* mdxFilePath ) {
-
-        {
-            // MDX ファイルパスが "" で括られている場合の補正
-            size_t len = strlen(mdxFilePath);
-            if (len > 0) {
-                if (mdxFilePath[0] == '\"' && mdxFilePath[len - 1] == '\"') {
-                    mdxFilePath[len - 1] = '\0';
-                    mdxFilePath++;
-                }
-            }
-
-            // MDX ファイルの読み込み
-//            printf("MDX:%s\n", mdxFilePath);
-            uint32_t mdxFileImageSizeInBytes = 0;
-            void *mdxFileImage = mallocReadFile(mdxFilePath, &mdxFileImageSizeInBytes);
-            if (mdxFileImage == NULL) {
-                printf("mallocReadFile '%s' failed.\n", mdxFilePath);
-                return false;
-            }
-
-            // MDX タイトルの取得
-            if (
-                MdxGetTitle(
-                    mdxFileImage, mdxFileImageSizeInBytes,
-                    mdxTitle, sizeof(mdxTitle)
-                ) == false
-            ) {
-                printf("MdxGetTitle failed.\n");
-                return false;
-            }
-            printf("TITLE:%s\n", mdxTitle);
-
-            // PDX ファイルを要求するか？
-            bool hasPdx;
-            if (
-                MdxHasPdxFileName(
-                    mdxFileImage, mdxFileImageSizeInBytes,
-                    &hasPdx
-                ) == false
-            ) {
-                printf("MdxHasPdxFileName failed.\n");
-                return false;
-            }
-
-            // PDX ファイルの読み込み
-            uint32_t pdxFileImageSizeInBytes = 0;
-            void *pdxFileImage = NULL;
-            if (hasPdx) {
-                char pdxFileName[FILENAME_MAX] = {0};
-                if (
-                    MdxGetPdxFileName(
-                        mdxFileImage, mdxFileImageSizeInBytes,
-                        pdxFileName, sizeof(pdxFileName)
-                    ) == false
-                ) {
-                    printf("MdxGetPdxFileName failed.\n");
-                    return false;
-                }
-                printf("%s\n", pdxFileName);
-
-        #ifdef _WIN32
-        #else
-                char mdxFilePathTmp[FILENAME_MAX] = {0};
-                strncpy(mdxFilePathTmp, mdxFilePath, sizeof(mdxFilePathTmp));
-                mdxFilePathTmp[FILENAME_MAX-1] = '\0';
-                const char *mdxDirName = dirname(mdxFilePathTmp);
-
-                /*
-                    ファイル名の大文字小文字が区別される環境では
-                        大文字ファイル名 + 大文字拡張子
-                        大文字ファイル名 + 小文字拡張子
-                        小文字ファイル名 + 大文字拡張子
-                        小文字ファイル名 + 小文字拡張子
-                    の 4 通りで PDX ファイル読み込みを試す必要がある。
-                */
-                for (int retryCount = 0; retryCount < 4; retryCount++) {
-                    char modifiedPdxFileName[FILENAME_MAX];
-                    memcpy(modifiedPdxFileName, pdxFileName, FILENAME_MAX);
-                    if (retryCount & 1) {
-                        /* ファイル名部分の大文字小文字反転 */
-                        for (char *p = modifiedPdxFileName; *p != '\0' && *p != '.'; p++) {
-                            if ('a' <= *p && *p <= 'z' || 'A' <= *p && *p <= 'Z') *p ^= 0x20;
-                        }
-                    }
-                    if (retryCount & 2) {
-                        /* 拡張子部分の大文字小文字反転 */
-                        char *p = modifiedPdxFileName;
-                        while (strchr(p, '.') != NULL) p = strchr(p, '.') + 1;
-                        for (; *p != '\0'; p++) {
-                            if ('a' <= *p && *p <= 'z' || 'A' <= *p && *p <= 'Z') *p ^= 0x20;
-                        }
-                    }
-
-                    char pdxFilePath[FILENAME_MAX];
-                    sprintf(pdxFilePath, "%s/%s", mdxDirName, modifiedPdxFileName);
-//                    printf("read %s ... ", pdxFilePath);
-                    pdxFileImage = mallocReadFile(pdxFilePath, &pdxFileImageSizeInBytes);
-                    if (pdxFileImage != NULL) {
-//                        printf("succeeded.\n");
-                        break;
-                    } else {
-                        printf("failed.\n");
-                    }
-                }
-        #endif
-            }
-
-            /* MDX PDX バッファの要求サイズを求める */
-            if (
-                MdxGetRequiredBufferSize(
-                    mdxFileImage,
-                    mdxFileImageSizeInBytes, pdxFileImageSizeInBytes,
-                    &mdxBufferSizeInBytes, &pdxBufferSizeInBytes
-                ) == false
-            ) {
-                printf("MdxGetRequiredBufferSize failed.\n");
-                return false;
-            }
-//			printf("mdxBufferSizeInBytes = %d\n", mdxBufferSizeInBytes);
-//			printf("pdxBufferSizeInBytes = %d\n", pdxBufferSizeInBytes);
-
-            /* MDX PDX バッファの確保 */
-            if (mdxBuffer != NULL) free(mdxBuffer);
-            mdxBuffer = (uint8_t *)malloc(mdxBufferSizeInBytes);
-            if (mdxBuffer == NULL) {
-                printf("malloc mdxBuffer failed.\n");
-                return false;
-            }
-            if (hasPdx) {
-                if (pdxBuffer != NULL) free(pdxBuffer);
-                pdxBuffer = (uint8_t *)malloc(pdxBufferSizeInBytes);
-                if (pdxBuffer == NULL) {
-                    printf("malloc pdxBuffer failed.\n");
-                    return false;
-                }
-            }
-
-            /* MDX PDX バッファを作成 */
-            if (
-                MdxUtilCreateMdxPdxBuffer(
-                    mdxFileImage, mdxFileImageSizeInBytes,
-                    pdxFileImage, pdxFileImageSizeInBytes,
-                    mdxBuffer, mdxBufferSizeInBytes,
-                    pdxBuffer, pdxBufferSizeInBytes
-                ) == false
-            ) {
-                printf("MdxUtilCreateMdxPdxBuffer failed.\n");
-                return false;
-
-            }
-
-            /* この時点でファイルイメージは破棄してよい */
-            if (pdxFileImage != NULL) free(pdxFileImage);
-            free(mdxFileImage);
-        }
-
-
-
-        // MXDRV の初期化
-        {
-            int ret = MXDRV_Start(
-                &context,
-                SAMPLE_RATE,
-                0, 0, 0,
-                MDX_BUFFER_SIZE,
-                PDX_BUFFER_SIZE,
-                0
-            );
-            if (ret != 0) {
-                printf("MXDRV_Start failed. return code = %d\n", ret);
-                return false;
-            }
-        }
-
-        /* PCM8 を有効化 */
-        uint8_t *pcm8EnableFlag = (uint8_t *)MXDRV_GetWork(&context, MXDRV_WORK_PCM8);
-        *(pcm8EnableFlag) = 1;
-
-        // 音量設定
-        MXDRV_TotalVolume(&context, 256);
-#if 0
-        // 再生時間を求める
-        {
-            float songDurationInSeconds = MXDRV_MeasurePlayTime(
-                &context,
-                mdxBuffer, mdxBufferSizeInBytes,
-                pdxBuffer, pdxBufferSizeInBytes,
-                1, 0
-            ) / 1000.0f;
-            printf("song duration %.1f(sec)\n", songDurationInSeconds);
-        }
-#endif
-
-
-        return true;    // NoError
-    }
-
-    bool close()
-    {
-        if (stream == 0)
-            return false;
-
-        PaError err = Pa_CloseStream( stream );
-        stream = 0;
-
-        MXDRV_End(&context);
-
-		if (pdxBuffer != NULL) {
-            free(pdxBuffer);
-            pdxBuffer = NULL;
-        }
-		if (mdxBuffer != NULL) {
-            free(mdxBuffer);
-            mdxBuffer = NULL;
-        }
-
-        return (err == paNoError);
-    }
-
-
-    bool start()
-    {
-        if (stream == 0) { return false;}
-
-        // MDX 再生
-        MXDRV_Play(
-            &context,
-            mdxBuffer, mdxBufferSizeInBytes,
-            pdxBuffer, pdxBufferSizeInBytes
-        );
-
-        PaError err = Pa_StartStream( stream );
-
-        return (err == paNoError);
-    }
-
-    bool stop()
-    {
-        if (stream == 0) { return false; }
-
-        PaError err = Pa_StopStream( stream );
-
-        return (err == paNoError);
-    }
-
-    bool fadeout()
-    {
-        MXDRV_Fadeout2( &context, 1 );
-
-        return true;
-    }
+    bool play();
+    bool stop();
+    bool fadeout();
 
 private:
-    // The instance callback, where we have access to every method/variable in object of class MDXPlayer 
     int paCallbackMethod(const void *inputBuffer, void *outputBuffer,
         unsigned long framesPerBuffer,
         const PaStreamCallbackTimeInfo* timeInfo,
-        PaStreamCallbackFlags statusFlags)
-    {
-
-        (void) timeInfo; // Prevent unused variable warnings. 
-        (void) statusFlags;
-        (void) inputBuffer;
-
-		MXDRV_GetPCM( &context, outputBuffer, framesPerBuffer );
-
-        return paContinue;
-    }
+        PaStreamCallbackFlags statusFlags);
 
     // This routine will be called by the PortAudio engine when audio is needed.
     // It may called at interrupt level on some machines so don't do anything
@@ -433,28 +70,13 @@ private:
         unsigned long framesPerBuffer,
         const PaStreamCallbackTimeInfo* timeInfo,
         PaStreamCallbackFlags statusFlags,
-        void *userData )
-    {
-        // Here we cast userData to MDXPlayer* type so we can call the instance method paCallbackMethod, we can do that since
-        //   we called Pa_OpenStream with 'this' for userData 
-        return ((MDXPlayer*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
-            framesPerBuffer,
-            timeInfo,
-            statusFlags);
-    }
+        void *userData );
 
 
-    void paStreamFinishedMethod()
-    {
-    }
+    void paStreamFinishedMethod();
 
-    //
     // This routine is called by portaudio when playback is done.
-    //
-    static void paStreamFinished(void* userData)
-    {
-        return ((MDXPlayer*)userData)->paStreamFinishedMethod();
-    }
+    static void paStreamFinished(void* userData);
 
 };
 
--- src/mxp.cpp
+++ src/mxp.cpp
@@ -1,27 +1,45 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <assert.h>
+#include <string.h>
+#include <libgen.h> // for dirname()
+#include <unistd.h>
+
 #include "mdxplayer.h"
 
 #define MXP_VERSION "0.02"
 
 // コマンドラインオプション
-struct MXPOPTION {
-	float	duration;
-	int		verbose;
-	char*	mdxFilePath;
+struct MXPOPTION
+{
+	float duration;
+	int verbose;
+	char *mdxFilePath;
 
-	MXPOPTION() : duration(0), verbose(0), mdxFilePath(NULL) {
+	MXPOPTION() : duration(0), verbose(0), mdxFilePath(NULL)
+	{
 		opterr = 0; // getopt()のエラーメッセージを無効にする。
 	}
 
-	bool getoption(int argc, char** argv)
+	bool getoption(int argc, char **argv)
 	{
 		int o;
-		while ((o = getopt(argc, argv, "vd:")) != -1) {
-			switch(o) {
-				case 'v':	verbose = 1;                       break;
-				case 'd':	duration = strtof(optarg, NULL);   break;
-				default:
-					usage();
-					return false;
+		while ((o = getopt(argc, argv, "vd:")) != -1)
+		{
+			switch (o)
+			{
+			case 'v':
+				verbose = 1;
+				break;
+			case 'd':
+				duration = strtof(optarg, NULL);
+				break;
+			default:
+				usage();
+				return false;
 			}
 		}
 
@@ -30,16 +48,20 @@ struct MXPOPTION {
 		return true;
 	}
 
-	bool check() {
-		if ( duration == 0.0f ) {
+	bool check()
+	{
+		if (duration == 0.0f)
+		{
 			duration = 100000.0f;
 		}
 
-		if (verbose) {
+		if (verbose)
+		{
 			printf("OPT:Duration: %4.2f\n", duration);
 		}
 
-		if (mdxFilePath == NULL) {
+		if (mdxFilePath == NULL)
+		{
 			printf("ERROR: Please specify an input mdx file.\n");
 			return false;
 		}
@@ -59,45 +81,43 @@ struct MXPOPTION {
 			"\tX68k MXDRV music driver version 2.06+17 Rel.X5-S (c)1988-92 milk.,K.MAEKAWA, Missy.M, Yatsube\n"
 			"\tConverted for Win32 [MXDRVg] V2.00a Copyright (C) 2000-2002 GORRY.\n"
 			"\tX68Sound_src020615 Copyright (C) m_puusan.\n"
-			"\tPorted for 64bit environments Copyright (C) 2018 Yosshin.\n\n"
-		);
-
+			"\tPorted for 64bit environments Copyright (C) 2018 Yosshin.\n\n");
 	}
 };
 
-
-
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    MXPOPTION		opt;
-    MDXPlayer       mdx;
-        
+	MXPOPTION opt;
+	MDXPlayer mdx;
+
 	printf("X68k MXDRV music driver version 2.06+17 Rel.X5-S (c)1988-92 milk.,K.MAEKAWA, Missy.M, Yatsube\n");
 	printf("music player version %s\n", MXP_VERSION);
+	fflush(stdout);
 
-	if ( opt.getoption( argc, argv ) == false )
+	if (opt.getoption(argc, argv) == false)
 	{
 		exit(EXIT_SUCCESS);
 	}
 
-	if ( opt.check() == false ) {
+	if (opt.check() == false)
+	{
 		exit(EXIT_FAILURE);
 	}
 
-    if ( mdx.open(Pa_GetDefaultOutputDevice()))
-    {
-        if ( mdx.load(opt.mdxFilePath) ) {
-            if ( mdx.start() )
-            {
-                Pa_Sleep( opt.duration * 1000 );
-                mdx.fadeout();
-                Pa_Sleep( 1000 );
-                mdx.stop();
-            }
-            mdx.close();
-        }
-    }
+	if (mdx.open())
+	{
+		if (mdx.load(opt.mdxFilePath))
+		{
+			if (mdx.play())
+			{
+				Pa_Sleep(opt.duration * 1000);
+				mdx.fadeout();
+				Pa_Sleep(1000);
+				mdx.stop();
+			}
+			mdx.close();
+		}
+	}
 
 	exit(EXIT_SUCCESS);
 }
-
